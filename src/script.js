e8bfe12cac5d4759c05aeea393f96c8b50b1b94b
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2021-11-08 22:35:40 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,6 +5,8 @@ import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer'
 import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass'
 import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 import * as dat from 'dat.gui'
+import fragment from './shader/fragment.glsl'
+import vertex from './shader/vertex.glsl'
 const canvas = document.querySelector('.webgl')
 
 class NewScene{
@@ -14,6 +16,7 @@ class NewScene{
     
     _Init(){
         this.scene = new THREE.Scene()
+        this.clock = new THREE.Clock()
         this.InitTextures()
         this.Globe()
         this.InitSettings()
@@ -53,16 +56,38 @@ class NewScene{
     }
 
     Globe(){
+        this.shaderMaterial = new THREE.ShaderMaterial({ 
+            fragmentShader: fragment,
+            vertexShader: vertex,
+            uniforms: {
+                u_time:  {value: 0},
+                u_texture1: { value: this.cloudTexture },
+                u_texture2: { value: this.colorTexture },
+                u_texture3: { value: this.lightTexture }
+            }
+        })
 
-        this.geometry = new THREE.SphereGeometry(50, 250, 250)
-        console.log(this.material)
-        this.world = new THREE.Mesh(this.geometry, new THREE.MeshStandardMaterial({ 
-            map: this.colorTexture,
-            transparent: true,
-            displacementMap: this.heightTexture,
-            displacementScale: 1.0
 
-        }))
+        this.number = 12 * 12
+        this.geometry = new THREE.BufferGeometry()
+        this.positions = new THREE.BufferAttribute(new Float32Array(this.number * 3), 3)
+        this.coordinates = new THREE.BufferAttribute(new Float32Array(this.number * 3), 3)
+
+        let index = 0
+        for (let i = 0; i < this.number; i++){
+            for(let j = 0; j < this.number; j++){
+                this.positions.setXYZ(index, i * 2, j * 2, 0)
+                this.coordinates.setXYZ(index,i,j,0)
+                index++
+            }
+        }
+
+        this.geometry.setAttribute('position', this.positions)
+        //console.log(this.positions)
+        this.geometry.setAttribute('aCoordinates', this.coordinates)
+
+        this.geometry = new THREE.SphereGeometry(50, 500, 500)
+        this.world = new THREE.Points(this.geometry, this.shaderMaterial)
         this.scene.add(this.world)
         this.world.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(this.world.geometry.attributes.uv.array, 2))
 
@@ -74,12 +99,12 @@ class NewScene{
     InitSettings(){
         this.settings = {
             progress: 0,
-            bloomStrength: 0.2,
+            bloomStrength: 3.0,
             bloomRadius: 0.9,
             bloomThreshold: 0.2
         }
         this.gui = new dat.GUI({closed: true})
-        this.gui.add(this.settings, 'progress', 0, 1, 0.01)
+        //this.gui.add(this.settings, 'progress', 0, 1, 0.01)
         this.gui.add(this.settings, 'bloomStrength', 0, 10, 0.01)
         this.gui.add(this.settings, 'bloomRadius', 0, 10, 0.01)
         this.gui.add(this.settings, 'bloomThreshold', 0, 10, 0.01)
@@ -89,24 +114,23 @@ class NewScene{
         this.renderScene = new RenderPass(this.scene, this.camera)
         this.bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.55, 2.5, 0.05)
         this.composer = new EffectComposer(this.renderer)
-        
+        this.composer.setSize(window.innerWidth, window.innerHeight)
         this.composer.addPass(this.renderScene)
-        
         this.composer.addPass(this.bloomPass)
     } 
 
 
     InitCamera(){
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 150)
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 500)
         this.camera.position.set(0, 0.5, 120)
         this.scene.add(this.camera)
     }
 
     InitLights(){
         this.ambientLight = new THREE.AmbientLight(0xffffff, 1)
-        this.pointLight = new THREE.PointLight(0xffffff, 1, 200, 125)
+        this.pointLight = new THREE.PointLight(0xffffff, 1000, 200, 125)
         this.scene.add(this.pointLight)
-        this.pointLight.position.set(0, 100, 0)
+        this.pointLight.position.set(0, 100, 100)
         this.scene.add(this.ambientLight)
     }
 
@@ -115,6 +139,7 @@ class NewScene{
             canvas,
             antialias: true,
         })
+        this.renderer.setClearColor(0x01152d)
         this.renderer.shadowMap.enabled = true
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
         this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -126,12 +151,16 @@ class NewScene{
         this.controls = new OrbitControls(this.camera, canvas)
         this.controls.enableDamping = true
         this.controls.update()
+        this.controls.enablePan = false
+        this.controls.enableZoom = false
     }
 
     Resize(){
         this.camera.aspect = window.innerWidth / window.innerHeight
         this.camera.updateProjectionMatrix()
         this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.composer.setSize(window.innerWidth, window.innerHeight)
+        this.composer.render()
     }
 
     Update(){
@@ -141,7 +170,8 @@ class NewScene{
                 this.bloomPass.strength = this.settings.bloomStrength
                 this.bloomPass.radius = this.settings.bloomRadius
                 this.composer.render(this.scene, this.camera)
-            }   
+            }
+            this.shaderMaterial.uniforms.u_time.value = this.clock.getElapsedTime() 
             //this.renderer.render(this.scene, this.camera)
             this.composer.render()
             this.controls.update()
